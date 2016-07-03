286cf88339f3975b42e6e974ef0ba3db14d1708c
Andrew Menning <menning23@gmail.com>
2016-07-03 19:30:33 -0400

    add url test for all GET pages

---
+++ src/AppBundle/Tests/ApplicationAvailabilityFunctionalTest.php
@@ -0,0 +1,57 @@
+<?php
+// src/AppBundle/Tests/ApplicationAvailabilityFunctionalTest.php
+namespace AppBundle\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ApplicationAvailabilityFunctionalTest extends WebTestCase
+{
+	/**
+	 * @dataProvider urlProvider
+	 */
+	 public function testGetPageIsSuccessful($url, $statusCode = 200, $targetUrlRegex = null)
+	 {
+	 	$client = self::createClient();
+		$client->request('GET', $url);
+		
+		if($statusCode === 200){
+			$this->assertTrue($client->getResponse()->isSuccessful());
+		} elseif ($statusCode === 301 || $statusCode === 302) {
+			$headers = $client->getInternalResponse()->getHeaders();
+			
+			$this->assertEquals($client->getResponse()->getStatusCode(), $statusCode);
+			$this->assertRegExp($targetUrlRegex,$headers["location"][0]);
+		} else {
+			var_dump($url);
+			var_dump($client->getResponse()->getStatusCode());
+			var_dump($client->getResponse()->getContent());
+			$this->assertTrue(FALSE);
+		}
+	 }
+	 
+	 public function urlProvider()
+	 {
+	 	return array(
+			array('/', 301, '/home/'),
+			array('/home', 302, '/login/'),
+			array('/login'),
+			array('/logout', 302, '/home/'),
+			array('/register', 301, '/register\//'),
+			array('/rules'),
+			array('/angular_directives/common-footer'),
+			array('/angular_directives/common-header'),
+			array('/angular_directives/game-active-dice'),
+			array('/angular_directives/game-board'),
+			array('/angular_directives/game-body'),
+			array('/angular_directives/game-frozen-dice'),
+			array('/angular_directives/game-grill-worms'),
+			array('/angular_directives/game-header'),
+			array('/angular_directives/game-login'),
+			array('/angular_directives/game-player-options'),
+			array('/angular_directives/game-player-worms'),
+			array('/angular_directives/game-registration'),
+			array('/angular_directives/game-setup'),
+			array('/angular_directives/tutorial-board'),
+		);
+	 }
+}
