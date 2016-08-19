9ee26c9e2bc8ea7ec20de710e6c0025f1fbd7278
Andrew Menning <menning23@gmail.com>
2016-08-18 21:20:34 -0400

    initial test spec

---
+++ spec/Resources/public/js/controllers/TestSpec.js
@@ -0,0 +1,52 @@
+/*
+describe('PasswordController', function() {
+  beforeEach(module('app'));
+
+  var $controller;
+
+  beforeEach(inject(function(_$controller_){
+    // The injector unwraps the underscores (_) from around the parameter names when matching
+    $controller = _$controller_;
+  }));
+
+  describe('$scope.grade', function() {
+    it('sets the strength to "strong" if the password length is >8 chars', function() {
+      var $scope = {};
+      var controller = $controller('PasswordController', { $scope: $scope });
+      $scope.password = 'longerthaneightchars';
+      $scope.grade();
+      expect($scope.strength).toEqual('strong');
+    });
+  });
+});
+*/
+/*
+"use strict";
+
+describe("SetWormImage service", function () {
+  var SetWormImage;
+
+  beforeEach(module("pickominoGame"));
+
+  beforeEach(inject(function (_SetWormImage_) {
+	  SetWormImage = _SetWormImage_;
+  }));
+
+  it("should return a filename", function () {
+    var response;
+	response = SetWormImage.imagify(21);
+	expect(response).not.toBe(null);
+  });
+
+});
+*/
+describe("Unit Testing Examples", function() {
+
+  beforeEach(angular.mock.module('pickominoGame'));
+
+  it('should have a working SetWormImage service', inject(['SetWormImage',
+    function(SetWormImage) {
+      expect(SetWormImage.imagify).not.to.equal(null);
+  	}
+  ]));
+});
