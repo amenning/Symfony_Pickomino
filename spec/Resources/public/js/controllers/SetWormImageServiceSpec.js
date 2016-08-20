6ad7ebd02fa00df9079dfac866c0ec9fd2ae3329
Andrew Menning <menning23@gmail.com>
2016-08-20 16:35:43 -0400

    full unit tests for SetWormImage service

--- spec/Resources/public/js/controllers/SetWormImageServiceSpec.js
+++ spec/Resources/public/js/controllers/SetWormImageServiceSpec.js
@@ -40,7 +40,18 @@ describe("SetWormImage service", function () {
 
 });
 */
-describe("Unit Testing Examples", function() {
+
+function using(name, values, func){
+  for (var i = 0, count = values.length; i < count; i++) {
+    if (Object.prototype.toString.call(values[i]) !== '[object Array]') {
+        values[i] = [values[i]];
+    }
+    func.apply(this, values[i]);
+    //jasmine.currentEnv_.currentSpec.description += ' (with "' + name + '" using ' + values[i].join(', ') + ')';
+  }
+};
+
+describe("SetWormImageServiceTests", function() {
   var SetWormImage;
   
   //beforeEach(module("pickominoGame"));
@@ -56,12 +67,47 @@ describe("Unit Testing Examples", function() {
   	}
   ]));
   
-  it('should return a filename', inject(['SetWormImage',
-   function(SetWormImage) { 
-	 var response;
-     response = SetWormImage.imagify(21); 
-	 expect(response).not.toBe(null);
-	 expect(response).toEqual("web/bundles/app/img/OneWormTile.png");
-   }
-  ]));
+  using("valid one worm tile values", [21, 22, 23, 24], function(value){
+	  it('should return a one worm tile image filename with value ' + value, inject(['SetWormImage',
+	   function(SetWormImage) { 
+		 var response;
+	     response = SetWormImage.imagify(value); 
+		 expect(response).not.toBe(null);
+		 expect(response).toEqual("web/bundles/app/img/OneWormTile.png");
+	   }
+	  ]));
+  });
+  
+  using("valid two worm tile values", [25, 26, 27, 28], function(value){
+	  it('should return a two worm tile image filename with value ' + value, inject(['SetWormImage',
+	   function(SetWormImage) { 
+		 var response;
+	     response = SetWormImage.imagify(value); 
+		 expect(response).not.toBe(null);
+		 expect(response).toEqual("web/bundles/app/img/TwoWormTile.png");
+	   }
+	  ]));
+  }); 
+  
+  using("valid three worm tile values", [29, 30, 31, 32], function(value){
+	  it('should return a three worm tile image filename with value ' + value, inject(['SetWormImage',
+	   function(SetWormImage) { 
+		 var response;
+	     response = SetWormImage.imagify(value); 
+		 expect(response).not.toBe(null);
+		 expect(response).toEqual("web/bundles/app/img/ThreeWormTile.png");
+	   }
+	  ]));
+  }); 
+  
+  using("valid four worm tile values", [33, 34, 35, 36], function(value){
+	  it('should return a four worm tile image filename with value ' + value, inject(['SetWormImage',
+	   function(SetWormImage) { 
+		 var response;
+	     response = SetWormImage.imagify(value); 
+		 expect(response).not.toBe(null);
+		 expect(response).toEqual("web/bundles/app/img/FourWormTile.png");
+	   }
+	  ]));
+  }); 
 });
