b3272870722bdde9e7459c4462d34bb7e0e95481
Andrew Menning <menning23@gmail.com>
2016-09-01 23:14:56 -0400

    add spies for spec

--- spec/Resources/public/js/services/BunkPenaltyServiceSpec.js
+++ spec/Resources/public/js/services/BunkPenaltyServiceSpec.js
@@ -8,19 +8,54 @@ function using(name, args, func){
 
 describe("BunkPenaltyServiceTests", function() {
   var BunkPenalty;
-  var $filter;
-  var FrozenDiceArray;
+  var PlayerWormsArray;
+  var GrillWormsArray;
+  var GameAction;
+  var RandomDice;
+  var PlayerNotification;
+  var GameState;
     
   //beforeEach(module("pickominoGame"));
   beforeEach(angular.mock.module('pickominoGame'));
 
-  beforeEach(inject(function (_BunkPenalty_) {
+  beforeEach(inject(function (
+		  _BunkPenalty_, 
+		  _PlayerWormsArray_, 
+		  _GrillWormsArray_,
+		  _GameAction_,
+		  _RandomDice_,
+		  _PlayerNotification_,
+		  _GameState_
+  ) {
 	  BunkPenalty = _BunkPenalty_;
+	  PlayerWormsArray = _PlayerWormsArray_;
+	  GrillWormsArray = _GrillWormsArray_;
+	  GameAction = _GameAction_;
+	  RandomDice = _RandomDice_;
+	  PlayerNotification = _PlayerNotification_;
+	  GameState = _GameState_;
+	  
+	  spyOn(RandomDice, 'resetDice').and.stub();
+	  spyOn(PlayerWormsArray, 'removeBunkWorm').and.stub();
+	  spyOn(GrillWormsArray, 'addWorm').and.stub();
+	  spyOn(GrillWormsArray, 'removeBunkWorm').and.stub();
+	  spyOn(GameAction, 'setStatus').and.stub();
+	  spyOn(PlayerNotification, 'setMessage').and.stub();
+	  spyOn(GameState, 'save').and.stub();
   }));
   
   
   it('should have a working BunkPenalty service', inject(['BunkPenalty', function(BunkPenalty) { 
 	  expect(BunkPenalty).toHaveMethod("penalize");
+   	}
+  ]));
+  
+  it('should call RandomDice.resetDice', inject(['BunkPenalty', 'PlayerWormsArray', 'GrillWormsArray', 'GameAction', 'RandomDice', 'PlayerNotification', 'GameState', 
+                                                 function(BunkPenalty, PlayerWormsArray, GrillWormsArray, GameAction, RandomDice, PlayerNotification, GameState
+    ) { 
+	  BunkPenalty.penalize();
+	  
+	  expect(RandomDice.resetDice).toHaveBeenCalled();
    	}
   ])); 
 });
