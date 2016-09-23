a116c8de8583dae7ec329e02e2250aef515fd614
Andrew Menning <menning23@gmail.com>
2016-09-22 23:32:08 -0400

    update description

--- spec/Resources/public/js/services/FreezeDiceActionServiceSpec.js
+++ spec/Resources/public/js/services/FreezeDiceActionServiceSpec.js
@@ -50,7 +50,7 @@ describe("FreezeDiceActionServiceTests", function() {
    	}
   ]));
   
-  it('should not freeze the dice if player has already frozen a dice grouping this turn and respond with message', 
+  it('should not freeze dice if player has already frozen a dice grouping this turn and respond with message', 
 	 inject(['FreezeDiceAction',
 	         'GameAction',
 	         'CheckValidDiceFreeze',
@@ -83,7 +83,7 @@ describe("FreezeDiceActionServiceTests", function() {
 	 		 }
   ]));
   
-  it('should not freeze the dice if player a player already has that dice value frozen and respond with message', 
+  it('should not freeze dice if player a player already has that dice value frozen and respond with message', 
 	 inject(['FreezeDiceAction',
 	         'GameAction',
 	         'CheckValidDiceFreeze',
